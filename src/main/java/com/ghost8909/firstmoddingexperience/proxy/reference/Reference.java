613eefe7f63f028108dcf8ac7ed90a71eb5b123c
Ghost8909 <Ghost8919@Gmail.com>
2015-10-18 23:35:29 -0400

    11:34 Oct 18, 2015
    Updates from Oct 18th

--- src/main/java/com/ghost8909/firstmoddingexperience/proxy/reference/Reference.java
+++ src/main/java/com/ghost8909/firstmoddingexperience/proxy/reference/Reference.java
@@ -5,4 +5,7 @@ public class Reference
 	public static final String MOD_ID = "FirstModdingExperience";
 	public static final String MOD_NAME = "First Modding Experience";
 	public static final String VERSION = "1.7.10-1.0";
+	public static final String CLIENT_PROXY_CLASS = "com.ghost8909.firstmoddingexperience.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.ghost8909.firstmoddingexperience.proxy.ServerProxy";
+	public static final String GUI_FACTORY_CLASS = "com.ghost8909.firstmoddingexperience.client.gui.GuiFactory";
 }
